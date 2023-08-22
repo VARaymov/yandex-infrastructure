9a9c7f7ebba623180cfb587e7639317e518b9986
Valery <student@fhmmr6vsrjf00j6d13ne.auto.internal>
2023-08-22 19:39:59 +0000

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-    # Описание бэкенда хранения состояния
+  # Описание бэкенда хранения состояния
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "terraform-state-std-019-002"
-    region     = "ru-central1"
-    key        = "terraform.tfstate"
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "terraform-state-std-019-002"
+    region   = "ru-central1"
+    key      = "terraform.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -35,8 +35,8 @@ data "yandex_vpc_network" "default" {
 }
 
 data "yandex_vpc_subnet" "default" {
-  name = "${data.yandex_vpc_network.default.name}-${var.network_zone}" 
-} 
+  name = "${data.yandex_vpc_network.default.name}-${var.network_zone}"
+}
 
 output "yandex_vpc_subnets" {
   description = "Yandex.Cloud Subnets map"
@@ -44,40 +44,39 @@ output "yandex_vpc_subnets" {
 }
 
 resource "yandex_compute_instance" "vm-1" {
-    name = "chapter5-lesson2-std-019-002"
+  name = "chapter5-lesson2-std-019-002"
 
-    # Конфигурация ресурсов:
-    # количество процессоров и оперативной памяти
-    resources {
-        cores  = 2
-        memory = 2
-    }
+  # Конфигурация ресурсов:
+  # количество процессоров и оперативной памяти
+  resources {
+    cores  = 2
+    memory = 2
+  }
 
-    # Загрузочный диск:
-    # здесь указывается образ операционной системы
-    # для новой виртуальной машины
-    boot_disk {
-        initialize_params {
-            image_id = "fd80qm01ah03dkqb14lc"
-        }
+  # Загрузочный диск:
+  # здесь указывается образ операционной системы
+  # для новой виртуальной машины
+  boot_disk {
+    initialize_params {
+      image_id = "fd80qm01ah03dkqb14lc"
     }
+  }
 
-    # Сетевой интерфейс:
-    # нужно указать идентификатор подсети, к которой будет подключена ВМ
-    network_interface {
-        subnet_id = "e9b3j9sag8o74g739e6v"
-        nat       = false
-    }
+  # Сетевой интерфейс:
+  # нужно указать идентификатор подсети, к которой будет подключена ВМ
+  network_interface {
+    subnet_id = "e9b3j9sag8o74g739e6v"
+    nat       = false
+  }
 
-    # Метаданные машины:
-    # здесь можно указать скрипт, который запустится при создании ВМ
-    # или список SSH-ключей для доступа на ВМ
-    metadata = {
-        ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-        cloud-init = file("${path.module}/cloud_init.cfg")
-    }
+  # Метаданные машины:
+  # здесь можно указать скрипт, который запустится при создании ВМ
+  # или список SSH-ключей для доступа на ВМ
+  metadata = {
+    user-data = file("${path.module}/meta.txt")
+  }
 }
 
 output "ip_address" {
-    value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
